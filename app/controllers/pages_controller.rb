ce6cc8634d3717cba9cd68aa24f4dac89e110a0b
Uthaeus <romanlavery@gmail.com>
2018-05-26 09:05:37 -0600

    Generated scaffold for blogs and created root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def fun
+  end
+
+  def misc
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get 'pages/fun'
+
+  get 'pages/misc'
+
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
